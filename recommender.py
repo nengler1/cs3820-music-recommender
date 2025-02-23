f8c403cdb71e6be2a9bc3da8fbcb1d56c4a2bb6a
Kroleski <rkroleski@comcast.net>
2025-02-22 23:15:47 -0700

    Second Push (from Kaggle Tutorial)

--- recommender.py
+++ recommender.py
@@ -26,11 +26,31 @@ warnings.filterwarnings("ignore")
 data = pd.read_csv("./data/data.csv")
 genre_data = pd.read_csv('./data/data_by_genres.csv')
 year_data = pd.read_csv('./data/data_by_year.csv')
-print(data.info())
-print(genre_data.info())
-print(year_data.info())
 
-data['decade'] = data['year'].apply(get_decade)
-sns.set(rc={'figure.figsize':(11 ,6)})
-sns.countplot(data['decade'])
-plt.show()
+cluster_pipeline = Pipeline([('scaler', StandardScaler()), ('kmeans', KMeans(n_clusters=10))])
+X = genre_data.select_dtypes(np.number)
+cluster_pipeline.fit(X)
+genre_data['cluster'] = cluster_pipeline.predict(X)
+
+tsne_pipeline = Pipeline([('scaler', StandardScaler()), ('tsne', TSNE(n_components=2, verbose=1))])
+genre_embedding = tsne_pipeline.fit_transform(X)
+projection = pd.DataFrame(columns=['x', 'y'], data=genre_embedding)
+projection['genres'] = genre_data['genres']
+projection['cluster'] = genre_data['cluster']
+fig = px.scatter(projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'genres'])
+fig.write_html("tsne.html")
+
+song_cluster_pipeline = Pipeline([('scaler', StandardScaler()), ('kmeans', KMeans(n_clusters=20, verbose=False))], verbose=False)
+X = data.select_dtypes(np.number)
+number_cols = list(X.columns)
+song_cluster_pipeline.fit(X)
+song_cluster_labels = song_cluster_pipeline.predict(X)
+data['cluster_label'] = song_cluster_labels
+
+pca_pipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=2))])
+song_embedding = pca_pipeline.fit_transform(X)
+projection = pd.DataFrame(columns=['x', 'y'], data=song_embedding)
+projection['title'] = data['name']
+projection['cluster'] = data['cluster_label']
+fig = px.scatter(projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'title'])
+fig.write_html("PCA.html")
